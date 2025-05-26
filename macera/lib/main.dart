c4fc40104231a2f992f356918a3866f327e2fc31
Your Name <you@example.com>
2025-05-26 12:47:47 +0300

    fix(update-check): handle null safety in version comparison for app updates
    - Changed `UpdateDataModel.version` and `storeLink` to be nullable
    - Updated `BusinessSettingsData` parsing logic to safely convert versions/links to string with `?.toString()`
    - Added null check before comparing `AppConfig.version` with updateData.version
    - Prevented redirect loop or unnecessary redirection if version is null

--- macera/lib/main.dart
+++ macera/lib/main.dart
@@ -116,8 +116,9 @@ var routes = GoRouter(
           if (extra is Map<String, dynamic>) {
             if (extra["skipUpdate"] == true) skipUpdate = true;
           }
-          if (AppConfig.version != AppConfig.businessSettingsData.updateData?.version 
-          && (!_isUpdateScreenOpened || !skipUpdate || AppConfig.businessSettingsData.updateData?.mustUpdate == true)
+          final UpdateDataModel? updateData = AppConfig.businessSettingsData.updateData;
+          if ((updateData?.version != null && AppConfig.version != updateData?.version) 
+          && (!_isUpdateScreenOpened || !skipUpdate || updateData?.mustUpdate == true)
           && state.uri.path != "/update") {
             _isUpdateScreenOpened = true;
             return '/update?url=${state.uri.path}';
